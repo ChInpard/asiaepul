9c5d30a52fad72f038a7c3ff2c2f2808d9f428f0
ChInpard <leeig0531@gmail.com>
2024-02-20 17:04:54 +0900

    20240220 LIG Spring first commit

---
+++ asiaepul/src/main/java/store/asiaepul/entity/DataModel.java
@@ -0,0 +1,17 @@
+package store.asiaepul.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class DataModel {
+	
+	private String variance;
+	private String category;
+	private String product;
+	private String mart;
+	private String peakTime;
+}
