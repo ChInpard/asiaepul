969b6712a941cd9f7707b4bd0dcc973ed0103e2c
ChInpard <leeig0531@gmail.com>
2024-02-27 15:41:34 +0900

    20240227 LIG 더미데이터를 통한 기능 구현 완료

--- asiaepul/src/main/resources/static/js/ranking-table.js
+++ asiaepul/src/main/resources/static/js/ranking-table.js
@@ -48,9 +48,11 @@ window.addEventListener("load", function (){
 	    
 	    rapidChaigingSalesTableHead.insertAdjacentHTML("beforeend", `
 	    	<tr class="bg-color:accent-1">
-                <th class="w:8% text-align:center">순위</th>
-                <th class="w:35% text-align:center">식품군</th>
-                <th class="w:35% text-align:center">식품</th>
+                <th class="text-align:center">순위</th>
+                <th class="text-align:center">식품군</th>
+                <th class="text-align:center">식품</th>
+                <th class="text-align:center">어제</th>
+                <th class="text-align:center">오늘</th>
                 <th class="text-align:center">증감률</th>
             </tr>
 	    `);
@@ -61,6 +63,8 @@ window.addEventListener("load", function (){
 	                <td class="text-align:center">${tableData[i].rank}</td>
 	                <td class="text-align:center">${tableData[i].category}</td>
 	                <td class="text-align:center">${tableData[i].product}</td>
+	                <td class="text-align:center">${tableData[i].yesterdayTotal}</td>
+	                <td class="text-align:center">${tableData[i].todayTotal}</td>
 	                <td class="text-align:center">${tableData[i].changeRate}</td>
 	            </tr>
 		    `;
--- asiaepul/src/main/resources/static/js/search-product.js
+++ asiaepul/src/main/resources/static/js/search-product.js
@@ -1,24 +1,77 @@
 window.addEventListener("load", function() {
 	const predictionSection = document.querySelector(".prediction-section");
 	
-	const productSearchForm = predictionSection.querySelector(".prediction-list form");
+	const productSearchForm = predictionSection.querySelector(".prediction-list .product-form");
+	const productSearchInput = productSearchForm.querySelector(".input-box .search-input");
 	const productList = predictionSection.querySelector(".product-list");
+	const productListSection = productList.querySelector(".contents");
+	
 	
 	const categoryButton = predictionSection.querySelector(".icon-category");
-	const productListSection = productList.querySelector(".contents");
 	
 	const nameSortButton = productList.querySelector(".product-name .icon-filter");
 	const demandSortButton = productList.querySelector(".demand .icon-filter");
 	
 	
-	categoryButton.addEventListener("click", async(e) => {
-	    e.preventDefault();
+	categoryButton.addEventListener("click", async() => {
 	    
 	    const popupContainer = document.querySelector(".popup-container");
+	    const searchText = popupContainer.querySelector(".popup .search-text");
+	    const categoryList = popupContainer.querySelector(".popup .popup-contents");
+	    
+	    /** icon-filter 클릭 시 보여지는 로직 */
+	    await searchCategories();
 	    
 	    // 팝업 컨테이너를 보이도록 설정
 	    popupContainer.style.display = 'block';
+	   	
 	    
+		/** 카테고리 검색 조회 로직 */
+		const categorySearchInput = popupContainer.querySelector(".popup .search-input");
+		categorySearchInput.addEventListener("click", async() => {
+		    await searchCategories();
+		    
+		    
+		});
+		const categorySearchText = popupContainer.querySelector(".popup .search-text");
+		categorySearchText.addEventListener("keydown", async(e) => {
+		    if (e.key === 'Enter') {
+		        await searchCategories();
+		    }
+		});
+		
+		async function searchCategories() {
+		    const query = searchText.value.trim();
+		    
+		    const url = `http://localhost:8000/categories?query=${query}`;
+		    const response = await fetch(url);
+		    const categoriesData = await response.json();
+		    console.log(categoriesData);
+		    
+		    categoryList.innerHTML = "";
+			for (let category of categoriesData) {
+				let template = `
+					<div class="content">${category}</div>
+				`;
+				categoryList.insertAdjacentHTML("beforeend", template);
+			}
+			// 각 카테고리에 클릭 이벤트 리스너
+			const categories = categoryList.querySelectorAll(".content");
+			categories.forEach(category => {
+			    category.addEventListener("click", async() => {
+			        const clickedCategory = category.textContent;
+			        console.log(clickedCategory);
+			        await searchProducts(clickedCategory);
+			        
+			        searchText.value = ""; // 검색 텍스트 초기화
+				    categoryList.innerHTML = ""; // 카테고리 목록 초기화
+				    
+				    // 팝업 컨테이너를 숨김 상태로 변경
+				    popupContainer.style.display = 'none';
+			    });
+			});
+            
+		}
 		
 	});
 	
@@ -26,64 +79,91 @@ window.addEventListener("load", function() {
 	const popupCloseButton = document.querySelector(".popup-close");
 	popupCloseButton.addEventListener("click", function() {
 	    const popupContainer = document.querySelector(".popup-container");
+	    const categoryList = popupContainer.querySelector(".popup .popup-contents");
+	    const searchText = popupContainer.querySelector(".popup .search-text");
+	    
+	    searchText.value = ""; // 검색 텍스트 초기화
+	    categoryList.innerHTML = ""; // 카테고리 목록 초기화
 	    
 	    // 팝업 컨테이너를 숨김 상태로 변경
 	    popupContainer.style.display = 'none';
 	});
 
 		
-	productSearchForm.addEventListener("submit", async(e) => {
+	productSearchInput.addEventListener("click", async(e) => {
 		e.preventDefault();
 		
-		const searchtext = predictionSection.querySelector(".search-bar .input-box .search-text");
-		const query = searchtext.value.trim();
-
-		const url = `http://localhost:8000/products?query=${query}`;
-		const response = await fetch(url);
-		const productsData = await response.json();
+		await searchProducts();
 		
-		// 데이터 동적 생성
+		const products = productListSection.querySelectorAll(".product");
+		changeStatus(products);
+	});
+	
+	
+	// 데이터 동적 생성
+	async function searchProducts(category) {
+		const searchText = predictionSection.querySelector(".search-bar .input-box .search-text");
+		const query = searchText.value.trim();
+		
+		let url = ``;
+		
+	    if (category) {
+			url = `http://localhost:8000/products?category=${category}`;
+	    } else {
+			url = `http://localhost:8000/products?query=${query}`;
+		}
+    	const response = await fetch(url);
+		const productsData = await response.json();
+		console.log(productsData);
+		
 		productListSection.innerHTML = "";
-        
 		for (let m of productsData) {
+			let statusColor = m.changeStatus === "▲" ? "main-1" : "main-2";
+			let plusOrMinus = m.changeStatus === "▲" ? "+" : "-";
+			
+			// 숫자를 콤마 단위로 포맷
+    		let formattedDemandPrediction = m.demandPrediction.toLocaleString();
+    
 			let template = `
                 <div class="product d:flex align-items:center">
                     <div class="product-name color:base-5">${m.name}</div>
-	                <div class="demand color:base-5 text-align:center">${m.demandPrediction}</div>
+	                <div class="demand color:base-5 text-align:center">${formattedDemandPrediction}</div>
                     <div class="toggle d:flex flex:col justify-content:center align-items:center">
-                        <span class="h:25px font-size:3 color:main-1">${m.changeStatus}</span>
-                        <span class="h:25px font-size:0 color:base-5">${m.changeRate}</span>
+                        <span class="h:25px font-size:3 color:${statusColor}">${m.changeStatus}</span>
+                        <span class="h:25px font-size:0 color:base-5"><span>${plusOrMinus}</span>${m.changeRate}</span>
                     </div>
         		</div>
 		    `;
 			productListSection.insertAdjacentHTML("beforeend", template);
 		}
+	}
 		
-		const products = productListSection.querySelectorAll(".product");
 		
-		// 제품 클릭 시 상태 변경
-        products.forEach(product => {
-            product.addEventListener("click", function() {
+	// 제품 클릭 시 상태 변경
+	function changeStatus(array) {
+        array.forEach(obj => {
+            obj.addEventListener("click", function() {
                 // 모든 다른 요소를 순회하며 클래스 변경
-                products.forEach(otherProduct => {
-                    if (otherProduct !== product) {
-                        otherProduct.classList.remove("clicked");
-                        otherProduct.classList.add("non-clicked");
+                array.forEach(otherObj => {
+                    if (otherObj !== obj) {
+                        otherObj.classList.remove("clicked");
+                        otherObj.classList.add("non-clicked");
                     }
                 });
 
                 // 현재 클릭된 요소의 클래스 변경
-                if (product.classList.contains("clicked")) {
-                    product.classList.remove("clicked");
-                    product.classList.add("non-clicked");
+                if (obj.classList.contains("clicked")) {
+                    obj.classList.remove("clicked");
+                    obj.classList.add("non-clicked");
                 } else {
-                    product.classList.remove("non-clicked");
-                    product.classList.add("clicked");
+                    obj.classList.remove("non-clicked");
+                    obj.classList.add("clicked");
                 }
             });
         });
-	});
-	
+    }
+        
+        
 	// 각각의 정렬 방식을 저장하는 변수
 	let currentSortMethod = ""; // 현재 선택된 정렬 방식
 	let nameSortDirection = "desc"; // 제품명 정렬 방식
